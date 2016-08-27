51ae5eeffe70a887e3c6bd8ec04a475d480453a7
pandavigoureux29 <pandavigoureux29@gmail.com>
2016-08-27 14:10:18 -0400

    keyboard event

--- Assets/scripts/inputs/KeyboardManager.cs
+++ Assets/scripts/inputs/KeyboardManager.cs
@@ -4,12 +4,12 @@ using System.Collections;
 /// <summary>
 /// This class is a prototype, not to be used on device
 /// </summary>
-public class BattleInputKeyboard : MonoBehaviour {
+public class KeyboardManager : MonoBehaviour {
 
-    [SerializeField] string m_inputAttack = "right";
-    [SerializeField] string m_inputDefend = "left";
+	[SerializeField] string m_inputRight = "right";
+	[SerializeField] string m_inputLeft = "left";
 
-    // Use this for initialization
+	// Use this for initialization
     void Start () {
 		if (Application.platform == RuntimePlatform.Android) {
 			enabled = false;
@@ -18,11 +18,21 @@ public class BattleInputKeyboard : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		CheckInput (m_inputRight);
+		CheckInput (m_inputLeft);
 	}
 
-	bool CheckInput( string _input, int _index){
+	bool CheckInput( string _input){
 		if (Input.GetKeyDown (_input)) {
-            
+			var player = GameObject.Find("Player").GetComponent<PlayerDebug>();
+			MoveCommand command = new MoveCommand ();
+			MoveCommandParameter param;
+			if (_input == m_inputRight) {
+				param = new MoveCommandParameter (MoveCommand.Direction.RIGHT, player);
+			} else {
+				param = new MoveCommandParameter (MoveCommand.Direction.LEFT, player);
+			}
+			CommandManager.instance.AddCommand (command,param);
 			return true;
 		} else if( Input.GetKeyUp(_input) )
         {
