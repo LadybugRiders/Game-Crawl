738d4d78b672bc84814322d24cd42df60834ad15
pandavigoureux29 <pandavigoureux29@gmail.com>
2016-08-28 15:21:22 -0400

    Crank sound before music

---
+++ Assets/scripts/music/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	[SerializeField] AudioSource m_crackSource;
+	[SerializeField] AudioSource m_musicSource;
+
+	bool m_musicLaunched = false;
+
+	public void Awake(){
+
+	}
+
+	public void StartMusic(){
+		m_crackSource.volume = 0.3f;
+		m_musicSource.Play ();
+	}
+
+}
---
+++ Assets/scripts/music/MusicTriggerer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicTriggerer : MonoBehaviour {
+
+	[SerializeField] MusicManager m_musicManager;
+	bool m_launched = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D _other){
+		
+		if (!m_launched && _other.gameObject.layer == LayerMask.NameToLayer ("Note")) {
+			m_launched = true;
+			m_musicManager.StartMusic ();
+		}
+	}
+}
