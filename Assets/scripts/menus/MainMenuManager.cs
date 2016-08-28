f24c0fd129496e6f34a764c4e4668588d4be7a68
pandavigoureux29 <pandavigoureux29@gmail.com>
2016-08-28 11:54:00 -0400

    main menu

--- Assets/scripts/menus/MainMenuManager.cs
+++ Assets/scripts/menus/MainMenuManager.cs
@@ -6,28 +6,15 @@ public class MainMenuManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (IsJustReleased ()) {
-			SceneManager.LoadScene ("grid");
-		}
 	}
 
-	bool IsJustReleased()
-	{
-		#if UNITY_STANDALONE || UNITY_EDITOR
-		return Input.GetMouseButtonUp(0);
-		#else
-		bool b = false;
-		for (int i = 0; i < Input.touches.Length; i++) {
-		b = Input.touches[i].phase == TouchPhase.Ended;
-		if (b)
-		break;
-		}
-		return b;
-		#endif
+	public void OnPlay(){
+		SceneManager.LoadScene ("grid");
 	}
+
 }
