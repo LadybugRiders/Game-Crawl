80d8ed510ac0420ed603a77ccd40dd79970ec457
Mefteg <gimenez.tom@gmail.com>
2016-08-28 20:33:54 -0400

    Clean and comment AStar

--- Assets/scripts/AStar.cs
+++ Assets/scripts/AStar.cs
@@ -9,7 +9,7 @@ public class AStar {
 		public float	cost;
 		public float	costWithHeuristic;
 
-		public uint		type;
+		public uint		data;
 
 		public Node() {
 			index = 0;
@@ -17,7 +17,7 @@ public class AStar {
 			cost = 0;
 			costWithHeuristic = 0;
 
-			type = 0;
+			data = 0;
 		}
 	}
 
@@ -33,7 +33,10 @@ public class AStar {
 		for (short i=0; i<nbNodes; i++) {
 			Node node = new Node();
 			node.index = i;
-			node.type = _grid[i];
+			// init the node with data
+			node.data = _grid[i];
+
+			// add the node to the grid
 			m_grid.Add(node);
 		}
 
@@ -48,15 +51,21 @@ public class AStar {
 		List<Node> openList = new List<Node>();
 		List<Node> closeList = new List<Node>();
 
+		// start with source as current node
 		Node currentNode = m_src;
 
 		uint infiniteLoopChecker = 0;
 		uint maxLoops = 1000000;
+		// while destination isn't reached
 		while(infiniteLoopChecker < maxLoops) {
+			// current node won't be evaluated anymore
 			closeList.Add(currentNode);
 
+			// if destination is reached
 			if (currentNode == m_dst) {
+				// construct the path
 				ConstructPath(m_grid, m_dst, _path);
+				// end the process
 				break;
 			}
 
@@ -64,6 +73,7 @@ public class AStar {
 			List<Node> neighbors = GetNeighbors(m_grid, currentNode);
 
 			int nbNeighbors = neighbors.Count;
+			// for each neighbor
 			for (int i=0; i<nbNeighbors; ++i) {
 				Node currentNeighbor = neighbors[i];
 				// if the neighbor isn't in the close list
@@ -83,6 +93,8 @@ public class AStar {
 						openList.Add(currentNeighbor);
 					}
 
+					// current neighbor is "valid"
+					// update it's data (cost + parent)
 					currentNeighbor.cost = tmpCost;
 					currentNeighbor.costWithHeuristic = tmpCost + GetHeuristic(currentNeighbor, m_dst);
 					currentNeighbor.parentIndex = currentNode.index;
@@ -91,7 +103,10 @@ public class AStar {
 
 			// update current node
 			currentNode = GetNodeWithLowerCostToDestination(openList);
+
+			// if the node with lower cost is an "invalid" node
 			if (currentNode.cost >= 1000) {
+				// no solution can be found
 				return false;
 			}
 
@@ -125,14 +140,21 @@ public class AStar {
 			neighbors.Add(neightbor);
 		}
 
+		// bottom
+		// we don't want to add the bottom neighbor because
+		// the ant can't go back
+
 		return neighbors;
 	}
 
 	float GetCost(Node node0, Node node1) {
-		if (node0.type == 0) {
+		// if the node is free (no note or bonus)
+		if (node0.data == 0) {
+			// give a correct cost
 			return node1.cost + 1;
 		}
 		else {
+			// otherwise provide a big cost to prevent astar to go on this node
 			return node1.cost + 1000;
 		}
 	}
@@ -144,16 +166,20 @@ public class AStar {
 		int line1	= node1.index / (int) m_nbColumns;
 		int column1 = node1.index % (int) m_nbColumns;
 
+		// use basic euclidian distance
 		return new Vector2(column1 - column0, line1 - line0).magnitude;
 	}
 
 	Node GetNodeWithLowerCostToDestination(List<Node> openList) {
+		// sort the open list with node with lower cost at the end
+		// (because it eases the last node removal)
 		openList.Sort(delegate(Node node0, Node node1) {
 			if (node0.costWithHeuristic == node1.costWithHeuristic) return 0;
 			else if (node0.costWithHeuristic < node1.costWithHeuristic) return 1;
 			else return -1;
 		});
 
+		// get the node and remove it from the list
 		Node nodeWithLowestCost = openList[openList.Count - 1];
 		openList.RemoveAt(openList.Count - 1);
 
@@ -161,11 +187,11 @@ public class AStar {
 	}
 
 	void ConstructPath(List<Node> grid, Node lastNode, List<Node> path) {
-
 		Node currentNode = lastNode;
 		bool again = true;
 		uint infiniteLoopChecker = 0;
 		uint maxLoops = 1000000;
+		// use nodes' parent to create the path
 		while (again && infiniteLoopChecker < maxLoops) {
 			path.Add(currentNode);
 			if (currentNode.parentIndex > -1) {
@@ -177,5 +203,8 @@ public class AStar {
 
 			++infiniteLoopChecker;
 		}
+
+		// reverse the path to get it in the right order (src -> dst)
+		path.Reverse();
 	}
 }
